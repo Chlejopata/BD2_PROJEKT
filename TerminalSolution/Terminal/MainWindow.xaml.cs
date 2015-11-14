bf5483bd18f180b583cf1adde5bfc2c969f039cc
Barkar19 <barkar19@gmail.com>
2015-11-14 20:28:49 +0100

    Główne okno, logo

--- TerminalSolution/Terminal/MainWindow.xaml.cs
+++ TerminalSolution/Terminal/MainWindow.xaml.cs
@@ -24,10 +24,5 @@ namespace Terminal
         {
             InitializeComponent();
         }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            test.Content = "Naciśnięto klawisz";
-        }
     }
 }
