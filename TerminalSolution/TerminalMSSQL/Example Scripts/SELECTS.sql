b93c1c8cc2a144526fd731405c67d347786b8617
Barkar19 <barkar19@gmail.com>
2015-12-08 16:15:56 +0100

    Coś się tu odejbało.

---
+++ TerminalSolution/TerminalMSSQL/Example Scripts/SELECTS.sql
@@ -0,0 +1,4 @@
+﻿SELECT * FROM [TerminalMSSQL].[dbo].[CONTACT_DATA];
+SELECT * FROM [TerminalMSSQL].[dbo].[AGENTS];
+SELECT * FROM [TerminalMSSQL].[dbo].[CLIENTS];
+SELECT * FROM [TerminalMSSQL].[dbo].[ACCOUNTS];
